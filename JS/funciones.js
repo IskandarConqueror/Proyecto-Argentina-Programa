c8586846c7a807cc15e5e4f4598d9fc8c364e91f
Miguel <kingiskandar33@gmail.com>
2023-02-28 18:00:25 -0300

    Agregado los datos faltantes.

--- JS/funciones.js
+++ JS/funciones.js
@@ -8,3 +8,10 @@ document.getElementById('boton2').onclick = function(){
 }
 }
 /**Al revisar esta zona, note que solo funcionaba en una sola instancia, probe haciendolo con un while y do while pero genero un bucle infinito. */
+
+window.onload = function (){
+    document.getElementById('boton3').onclick = function() {
+    console.log("Hicimos Click");
+    document.getElementById("lugar").hidden="true";
+} 
+}
