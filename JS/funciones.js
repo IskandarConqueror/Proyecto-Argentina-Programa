4c42388647b85eb9737d8cccf110b43b3c0d8380
Miguel <kingiskandar33@gmail.com>
2023-03-24 02:15:59 -0300

    Reparado

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,17 +1,16 @@
-window.onload = function (){
-    document.getElementById('boton').onclick = function() {
-    console.log("Hicimos Click");
-    document.getElementById("email").innerHTML="darrell.carroll@example.com";
-} 
-document.getElementById('boton2').onclick = function(){
-    document.getElementById("email").hidden="True";
-}
-}
-/**Al revisar esta zona, note que solo funcionaba en una sola instancia, probe haciendolo con un while y do while pero genero un bucle infinito. */
+/**
 
-window.onload = function (){
-    document.getElementById('boton3').onclick = function() {
-    console.log("Hicimos Click");
-    document.getElementById("lugar").hidden="true";
-} 
-}
+const emailBtn = document.getElementById('email');
+const email = document.getElementById('bemail');
+
+emailBtn.addEventListener('click', function() {
+  if (email.style.display === 'none') {
+    email.style.display = 'block';
+    emailBtn.textContent = 'Ocultar Email';
+  } else {
+    email.style.display = 'none';
+    emailBtn.textContent = 'Mostrar Emails';
+  }
+});
+
+Al revisar esta zona, note que solo funcionaba en una sola instancia, probe haciendolo con un while y do while pero genero un bucle infinito. */
